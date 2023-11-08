d40c0042699aa578dd4ae3103fbd34e7bb45c9d5
Shen-Kuo <jiashenkuo@gmail.com>
2023-11-08 16:02:58 -0500

    split schema, removed redundant cost, removed redundant metadata, update prompts

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,3 @@
-import huggingface_hub
 import openai
 from collections import Counter
 from langchain.text_splitter import MarkdownHeaderTextSplitter
@@ -19,6 +18,7 @@ import asyncio
 import sys
 import os
 
+from utils import hf_utils
 import keys
 import prompt
 
@@ -47,76 +47,22 @@ with open("log.txt", 'w') as the_log:
     the_log.write("")
     the_log.close()
 
-def load_card(model_name: str) -> str:
+def get_data_schema() -> dict:
 
-    try:
-        card = huggingface_hub.ModelCard.load(model_name)
-    except huggingface_hub.utils._errors.RepositoryNotFoundError:
-        print(f"{model_name} repo not found\n")
-        #card.data = tags
-        #card.text card
-        #cald.content tags+cards
-    return ("#tags\n\n" + card.content)
-    
-def get_type_and_task(model_name: str) -> (str,str):
-    model_info = huggingface_hub.hf_api.model_info(model_name)
-    tags = model_info.tags
-    model_task = None
-    model_type = None
-
-    tasks = {
-    "multimodal":["feature-extraction", "text-to-image", "image-to-text", "text-to-video", "visual-question-answering", "document-question-answering", "graph-machine-learning"],
-    "computer-vision":["depth-estimation", "image-classification", "object-detection", "image-segmentation", "image-to-image", "unconditional-image-generation", "video-classification", "zero-shot-image-classification"],
-    "natural-language-processing":["text-classification", "token-classification", "table-question-answering", "question-answering", "zero-shot-classification", "translation", "summarization", "conversational", "text-generation", "text2text-generation", "fill-mask", "sentence-similarity"], 
-    "audio":["text-to-speech", "automatic-speech-recognition", "audio-to-audio", "audio-classification", "voice-activity-detection"],
-    "tabular":["tabular-classification", "tabular-regression"],
-    "reinforcement-learning":["reinforcement-learning", "robotics"]
-    }
-
-    for tag in tags:
-        for key in tasks.keys():
-            if tag in tasks[key]:
-                model_type = key
-                model_task = tag
-    return (model_type, model_task)
-
-def get_frameworks(model_name: str) -> list:
-    model_info = huggingface_hub.hf_api.model_info(model_name)
-    tags = model_info.tags
-
-    frameworks = ["pytorch", "tensorflow", "jax", "transformers", "tensorboard", "diffusers", "stable-baselines3", "safetensors", "peft", "onnx", 
-                  "ml-agents", "sentence-transformers", "timm", "sample-factory", "keras", "adapter-transformers", "flair", "spacy", "espnet", 
-                  "transformers.js", "fastai", "core-ml", "rust", "nemo","joblib","scikit-learn","fasttext","speechbrain","paddlepaddle","openclip",
-                  "bertopic", "fairseq", "openvino", "graphcore", "stanza", "tf-lite", "asteroid", "paddlenlp", "allennlp", "spanmarker", "habana", 
-                  "pythae", "pyannote.audio" ]
-
-    return list(set(tags) & set(frameworks))
-
-def get_data_schema(model_type: str, model_tasks: list, frameworks: list) -> (dict, dict):
-    general_schema = dict(schema["general_metadata"])
-    general_schema.pop("model_type")
-    general_schema.pop("model_tasks")
-    general_schema.pop("frameworks")
-
-    pre_extraction_data = {"model_type": model_type, 
-                           "model_tasks": model_tasks,
-                           "frameworks": frameworks
-                           }
-
-    if model_type:
-        task_specific_schema = dict(schema["task_specific_metadata"][model_type])
-        metadata_schema = {
-            "properties": {**general_schema, **task_specific_schema}
-            }
-        return (pre_extraction_data, metadata_schema)
-    return (pre_extraction_data, {"properties": general_schema})
+    simple_metadata_schema = {"simple":{"properties": {**schema["simple_metadata"]}}}
+    complex_metadata_schema = {"complex":{"properties": {**schema["complex_metadata"]}}}
+    return {**simple_metadata_schema, **complex_metadata_schema}
 
 def pretty_print_docs(docs, metadata):
     return f"{'-' * 20} {metadata} {'-' * 20}\n\n" + f"\n{'-' * 30}\n".join([f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]) + "\n\n"
 
 def log(text: str):
     with open("log.txt", 'a') as the_log:
-        the_log.write(text)
+        if(type(text) == str):
+                the_log.write(text)
+        elif(type(text) == dict):
+            for key, value in text.items():
+                the_log.write(f"{key}: {value}\n")
         the_log.close()
 
 def log_list(content: str):
@@ -128,8 +74,9 @@ def log_list(content: str):
         file.close()
     
 
-log("Metadata Prompt: \n\n " + str(prompt.METADATA_PROMPT) + "\n")
-log("Extraction Prompt: \n\n" + str(prompt.EXTRACTION_PROMPT) + "\n")
+log("Metadata Prompt: \n\n")
+log(prompt.METADATA_PROMPT)
+log("\nExtraction Prompts: \n\n" + str(prompt.SIMPLE_METADATA_EXTRACTION_PROMPT) + str(prompt.COMPLEX_METADATA_EXTRACTION_PROMPT) + "\n")
 
 if args.data == "input":
     models_iterable = data
@@ -141,69 +88,83 @@ if args.data == "filtered":
     models_iterable = data[start_index: end_index]
 
 start_time = time.time()
-result = {}
+final_result = {}
+
+extraction_prompt = {
+    "simple": ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(content = (prompt.SIMPLE_METADATA_EXTRACTION_PROMPT)),
+            HumanMessagePromptTemplate.from_template("{documents}"),
+        ]
+    ),
+    "complex": ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(content = (prompt.COMPLEX_METADATA_EXTRACTION_PROMPT)),
+            HumanMessagePromptTemplate.from_template("{documents}"),
+        ]
+    ),
+}      
 
 #change between filtered_models and input_models
 for model in models_iterable:
-    try:
-        card = load_card(model)
-        card = card.replace('"', "'")
-        log(f"\n#####################{model}########################\n\n")
-        model_type, model_tasks = get_type_and_task(model)
-        frameworks = get_frameworks(model)
-        pre_extract, data_schema = get_data_schema(model_type, model_tasks, frameworks)
+    model_result = {}
+    # try:
+    card = hf_utils.load_card(model)
+    card = card.replace('"', "'")
+    log(f"\n#####################{model}########################\n\n")
+    model_result["domain"], model_result["model_tasks"] = hf_utils.get_domain_and_task(model)
+    model_result["frameworks"]= hf_utils.get_frameworks(model)
+    model_result["libraries"]= hf_utils.get_libraries(model)
+    data_schema = get_data_schema()
 
-        headers_to_split_on = [("#", "header 1"), ("##", "header 2"), ("###", "header 3")]
-        markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on = headers_to_split_on)
-        md_header_splits = markdown_splitter.split_text(card)
-        #print(md_header_splits)
+    headers_to_split_on = [("#", "header 1"), ("##", "header 2"), ("###", "header 3")]
+    markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on = headers_to_split_on)
+    md_header_splits = markdown_splitter.split_text(card)
+    #print(md_header_splits)
 
-        vector_store = FAISS.from_documents(md_header_splits, OpenAIEmbeddings(allowed_special={'<|endoftext|>', '<|prompter|>', '<|assistant|>'}))
-        llm = OpenAI(temperature = 0, model_name = "gpt-3.5-turbo")
+    vector_store = FAISS.from_documents(md_header_splits, OpenAIEmbeddings(allowed_special={'<|endoftext|>', '<|prompter|>', '<|assistant|>'}))
+    llm = OpenAI(temperature = 0)
+    chatbot = ChatOpenAI(temperature = 0.1, model = "gpt-3.5-turbo")
+
+
+    for key in data_schema.keys():
         compressed_docs = ""
-        for metadata in data_schema["properties"]:
+        for metadata in data_schema[key]["properties"]:
             retriever = vector_store.as_retriever(search_kwargs = {"k": 3})
             retriever_prompt = prompt.METADATA_PROMPT[metadata]
-            # docs = list of doc objects
-            docs = retriever.get_relevant_documents(retriever_prompt)
+            #log(pretty_print_docs(docs, metadata))
             compressor = LLMChainExtractor.from_llm(llm)
             compression_retriever = ContextualCompressionRetriever(base_compressor = compressor, base_retriever = retriever)
-            compressed_docs += pretty_print_docs(compression_retriever.get_relevant_documents(retriever_prompt + f" (Do not remove keyword \"{metadata}\" in compressed doc)"), metadata)
-        log(compressed_docs)
-
-        prompt_template = ChatPromptTemplate.from_messages(
-            [
-                SystemMessage(content = (prompt.EXTRACTION_PROMPT)),
-                HumanMessagePromptTemplate.from_template("{documents}"),
-            ]
-        )
-
-        chatbot = ChatOpenAI(temperature = 0.1, model = "gpt-3.5-turbo")
-        chain = create_extraction_chain(schema = data_schema, llm = chatbot, prompt = prompt_template)
-
+            single_compressed_docs = compression_retriever.get_relevant_documents(retriever_prompt + f" Keep surrounding context in compressed document.")
+            compressed_docs += pretty_print_docs(single_compressed_docs, "compressed" + metadata)
+            log(pretty_print_docs(single_compressed_docs, "compressed " + metadata))    
+        chain = create_extraction_chain(schema = data_schema[key], llm = chatbot, prompt = extraction_prompt[key])
         extraction_result = chain.run({
-            "model_type": model_type,
+            "domain": model_result["domain"],
             "model": model,
             "documents": compressed_docs
             })
-
-        
-        log_list("\n" + str(extraction_result))
+    
         if type(extraction_result) == list:
-            result[model] = {**pre_extract, **extraction_result[0]}
-        else:
-            result[model] = {**pre_extract, **extraction_result}
+            model_result = {**model_result,**extraction_result[0]}
+        if type(extraction_result) == dict:
+            model_result = {**model_result,**extraction_result}
+
+    
+    print(model_result)
+    log_list("\n" + str(model_result))
+    final_result[model] = model_result
 
     #exceed token limit of 4096
-    except Exception as e:
+    # except Exception as e:
 
-        log(f"\nERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n {str(e)} \n")
-        result[model] = str(e)
+    #     log(f"\nERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n {str(e)} \n")
+    #     final_result[model] = str(e)
         
 
     #For eval purposes
     with open("result.json", "w") as json_file:
-        json.dump(result, json_file, indent = 4)
+        json.dump(final_result, json_file, indent = 4)
 
 
 # file_path = "result.json"
--- prompt.py
+++ prompt.py
@@ -1,43 +1,45 @@
 METADATA_PROMPT = {
-    "datasets": "What datasets were used to train the model (include link if possible)", # eval: 
-    "license": "What is the license", # eval: 
-    "github": "What links to github repositories are available and what these links are for", # eval: 
-    "paper": "What is the research paper associated to this model", # eval: 
-    "upstream_model": "What is the upstream model of this model", # eval:
-    "parameter_count": "What are the number of parameters (#params) this model is trained on", # eval: 
+    "datasets": "What datasets, provide url if possible, were the model trained/pretrained on?" , # eval: 
+    "license": "What is the license?", # eval: 
+    "github": "What urls to github repositories are available and what these urls are for?", # eval: 
+    "paper": "What is the research paper url associated to this model?", # eval: 
+    "upstream_model": "What model was this model pretrained or downstreamed from?", # eval:
+    "parameter_count": "What are the number of parameters (#params) this model is trained on, sometimes represented with \"M\", \"B\", and \"T\" as million, billion, and trillion?", # eval: 
     "hyper_parameters": "What are the values of some hyper parameters (parameters that control the learning process of the model) of this model.", # eval: 
-    "evaluation": "What is the evalutaion of the model. What evaluation metrics where used, and what are the results (include whole table if possible)", # eval: 
-    "hardware": "What hardware was used to train this model", # eval: 
-    "limitation_and_bias": "What are the limitations and biases of the model", # eval: 
-    "demo": "Find a form of demo for the model could be a link, code snippet or short paragraph", # eval: 
-    "input_format": "What is the format of the data used as input for the model", # eval: 
-    "output_format": "What is the format of the data used as output of the model", # eval: 
-    "input_preprocessing": "What is the input preprocessing of this model", # eval: 
-    "input_size": "What is the image input size",  # eval:  
-    "max_sequence_length": "What is the max sequence length of this NLP model", # eval: 
-    "vocabulary_size": "What is the vocabulary size of this NLP model", # eval: 
-    "sample_rate": "What is the sample rate of this model", # eval: 
-    "agent": "What is the agent of this reinforcement learning model", #eval:
-    "training_environment": "What is the training environment of this reinforcement learning model", #eval:
-    "SB3": "Is SB3 used in this reinforcement learning model", #eval:
+    "evaluation": "What is the evalutaion of the model. What evaluation metrics where used, and what are the results (include whole table if possible)?", # eval: 
+    "hardware": "What hardware (GPU and TPU pods) was used to train this model?", # eval: 
+    "limitation_and_bias": "What are the limitations and biases of the model?", # eval: 
+    "demo": "Find a form of demo for the model could be a link, code snippet or short paragraph.", # eval: 
+    "input_format": "What is the format of the data used as input for the model?", # eval: 
+    "output_format": "What is the format of the data used as output of the model?", # eval: 
 }
 
-EXTRACTION_PROMPT = \
-    "Given metadata information of huggingface {model_type} model : {model}, extract the properties of ONE single entity mentioned in the 'information_extraction' function.\n \
+SIMPLE_METADATA_EXTRACTION_PROMPT = \
+    "Given metadata information of huggingface {domain} model : {model}, extract the properties of ONE single entity mentioned in the 'information_extraction' function.\n \
     Extraction rules: \n \
-    - rule 1: Adhere strictly to the schema structure in 'information_extraction'\n \
-    - rule 2: If a property is not present but is required in the function parameters, output "" instead\n \
+    - rule 1: Adhere strictly to the schema structure and in 'information_extraction'\n \
+    - rule 2: If a property is not present but is required in the function parameters, output empty string instead\n \
     - rule 3: If a property is not present and is not required in the function parameters, do not include it in the output\n \
-    - rule 4: Only extract one item for 'info' in  'information_extraction' function \n \
-    Extraction rules for specific metadata: \n \
+    - rule 4: Only extract one item for 'info' in 'information_extraction' function \n \
+    Extraction rules for metadata: \n \
     - datasets: only return dataset used to train or finetune model, not the upstream model of the model\n \
     - github: extract github link of this model (only return the github link)\n \
     - paper: if a research paper was written, extract arxiv research paper link (only return the url of the paper)\n \
-    - parameter_count: The number of parameters the model was trained on, sometimes represented in the form #params\n \
+    - parameter_count: output he number of parameters\n \
     - upstream_model: provide huggingface model ID of upstream model\n \
-    - hyper_parameters: extract possible hyperparameters\n \
-    - evaluation: extract evaluation metric/tasks and their respective evaluation results\n \
     - hardware: extract any hardware used to train the model\n \
+"
+
+COMPLEX_METADATA_EXTRACTION_PROMPT = \
+    "Given metadata information of huggingface {domain} model : {model}, extract the properties of ONE single entity mentioned in the 'information_extraction' function.\n \
+    Extraction rules: \n \
+    - rule 1: Adhere strictly to the schema structure in 'information_extraction'\n \
+    - rule 2: If a property is not present but is required in the function parameters, output empty string instead\n \
+    - rule 3: If a property is not present and is not required in the function parameters, do not include it in the output\n \
+    - rule 4: Only extract one item for 'info' in 'information_extraction' function \n \
+    Extraction rules for metadata: \n \
+    - hyper_parameter: extract possible hyperparameters\n \
+    - evaluation: extract evaluation metric/tasks and their respective evaluation results\n \
     - limitation_and_biases: extract a short summary of limitation and biases of the model\n \
     - demo: extract any links, code snippets, or small paragraphs on how to use the model\n \
     - input_format: extract the input format/requirement for this model\n \
